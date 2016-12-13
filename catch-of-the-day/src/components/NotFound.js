fc328faf27ae892ff7a894897cc524d6243788f8
Mike Borsare <mborsare@gmail.com>
2016-12-12 20:37:05 -0500

    Learned about routing

---
+++ catch-of-the-day/src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class NotFound extends React.Component {
+  render() {
+    return (
+      <p>This is the NotFound component</p>
+    )
+  }
+}
+
+export default NotFound;
