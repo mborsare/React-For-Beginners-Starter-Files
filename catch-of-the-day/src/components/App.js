6fbf5a0f6251e55b0a2efc302307382ec5bb4e66
Mike Borsare <mborsare@gmail.com>
2017-03-24 16:17:20 -0400

    Working through state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,27 @@ import Order from "./Order";
 import Inventory from "./Inventory";
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.addFish = this.addFish.bind(this);
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+
+  addFish(fish) {
+    // update state
+    const fishes = {...this.state.fishes};
+
+    // add in our new fish
+    const timestamp = Date.now();
+    fishes [`fish-#{timestamp}`] = fish;
+
+    // set state
+    this.setState({ fishes });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -12,7 +33,7 @@ class App extends React.Component {
         </div>
 
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish}/>
       </div>
     )
   }
